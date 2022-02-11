ab17d3dace0c376af2863fce90b460925a080d4c
andrelopes301 <andrerlopes00@gmail.com>
2022-02-11 15:23:41 +0000

    Updates

---
+++ precache-manifest.8c557cc4352c4e22a4119cb80343140a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e3cbbe32f7e156f55d9d17e1a662408",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efd124c9b74175ab48db",
+    "url": "/static/css/main.82458425.chunk.css"
+  },
+  {
+    "revision": "3a33124c704945b257b7",
+    "url": "/static/js/2.ef72e1c4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.ef72e1c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efd124c9b74175ab48db",
+    "url": "/static/js/main.25094196.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "57f4bcb94ef62eca1dd306ef7eb3c192",
+    "url": "/static/media/perfil.57f4bcb9.png"
+  },
+  {
+    "revision": "a74fb2596c0df164d090ad1b4a41958b",
+    "url": "/static/media/resume.a74fb259.pdf"
+  }
+]);
